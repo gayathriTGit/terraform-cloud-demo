1f62132efbf723ed8074264ce6170686c15b9e58
gayathriT <98060548+gayathriTGit@users.noreply.github.com>
2025-04-17 15:37:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,11 @@ filter {
 
 }
 
-resource "aws_instance" "myec3" {
+resource "aws_instance" "myec4" {
   ami    = data.aws_ami.myami.id 
   instance_type = "t2.micro"
 tags = {
-    Name = "dev-env-03"
+    Name = "dev-env-04"
   }
  
 }
